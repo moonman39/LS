d335384d7656f75bf18fdc2da29d0597b5a26cf8
moonman39 <grant.luna@fnf.com>
2022-08-08 18:28:37 -0700

    Create calculator code prior to watching walkt-through

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,62 @@
+# Pre-Video Code
+
+def integer_check(input)
+  conversion = input.to_i
+  conversion.to_s == input 
+end
+
+def input_to_number(input)
+  number = input.to_i
+end
+
+valid_operators = ['+', "-", "/", "*", "%"]
+converted_number_1 = nil
+converted_number_2 = nil
+operation = nil
+
+
+loop do
+  loop do
+    puts "\nPlease enter your first number:"
+    number_1 = gets.chomp
+    converted_number_1 = input_to_number(number_1).to_f
+
+    puts "\nPlease enter your second number:"
+    number_2 = gets.chomp
+    converted_number_2 = input_to_number(number_2)
+
+    break if integer_check(number_1) && (integer_check(number_2) && converted_number_2 != 0)
+
+    puts "Invalid input! Make sure you enter two integers"
+  end
+
+  loop do
+    puts "\nPlease enter your desired operation:"
+    operation = gets.chomp
+
+    break operation if valid_operators.include?(operation)
+
+    puts "Invalid input! Please enter +, -, *, /, or %"
+  end
+
+
+  case operation
+    when "+"  then puts converted_number_1 + converted_number_2
+    when "-"  then puts converted_number_1 - converted_number_2
+    when "*"  then puts converted_number_1 * converted_number_2
+    when "/"  then puts converted_number_1 / converted_number_2
+    when "%"  then puts converted_number_1 % converted_number_2
+  end
+
+  break
+end
+
+
+
+# Video Code
+
+
+
+
+
+
