76a5c167ed8ef9a69b4f04b7940b4609c3ec03d4
moonman39 <grant.luna@fnf.com>
2022-08-19 00:00:30 -0700

    Refactored code

--- rb101-programming_foundations/lesson_6/twenty-one.rb
+++ rb101-programming_foundations/lesson_6/twenty-one.rb
@@ -1,43 +1,32 @@
-# Pseudo-Code
-# 1. Initialize deck
-# 2. Deal cards to player and dealer
-# 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-# 4. If player busts, dealer wins
-# 5. Dealer turn: hit or stay
-#   - repeat until total >= 17
-# 6. If dealer bust, player wins.
-# 7. Compare cards and declare winner.
+# Figure out how to display the winner - you may have to refactor a bit
 
-# Data Stuctures
-deck = [['2', 'H'], ['3', 'H'], ['4', 'H'], ['5', 'H'], ['6', 'H'], ['7', 'H'], ['8', 'H'], ['9', 'H'],
-        ['10', 'H'], ['Jack', 'H'], ['Queen', 'H'], ['King', 'H'], ['Ace', 'H'], ['2', 'S'], ['3', 'S'], ['4', 'S'], 
-        ['5', 'S'], ['6', 'S'], ['7', 'S'], ['8', 'S'], ['9', 'S'], ['10', 'S'], ['Jack', 'S'], ['Queen', 'S'], 
-        ['King', 'S'], ['Ace', 'S'], ['2', 'D'], ['3', 'D'], ['4', 'D'], ['5', 'D'], ['6', 'D'], ['7', 'D'], 
-        ['8', 'D'], ['9', 'D'], ['10', 'D'], ['Jack', 'D'], ['Queen', 'D'], ['King', 'D'], ['Ace', 'D'], ['2', 'C'], 
-        ['3', 'C'], ['4', 'C'], ['5', 'C'], ['6', 'C'], ['7', 'C'], ['8', 'C'], ['9', 'C'], ['10', 'C'], 
-        ['Jack', 'C'], ['Queen', 'C'], ['King', 'C'], ['Ace', 'C']]
-
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King',
+          'Ace']
 FACE_CARDS = %w(Jack Queen King)
 
-def prompt message
+def initialize_deck
+  VALUES.product(SUITS)
+end
+
+def prompt(message)
   puts "\n=> #{message}"
 end
 
-def random_card deck
+def random_card(deck)
   card = deck.sample
   deck.delete(card)
   card.first
 end
 
-def initial_deal player_cards, dealer_cards, deck
+def initial_deal(player_cards, dealer_cards, deck)
   2.times do
     player_cards << random_card(deck)
     dealer_cards << random_card(deck)
   end
 end
 
-def deal card_set, deck
+def deal(card_set, deck)
   card_set << random_card(deck)
 end
 
@@ -51,29 +40,28 @@ def hit_or_stay
   choice
 end
 
-def initial_player_display card_set
+def initial_player_display(card_set)
   print "\n=> Your cards are "
   sleep 1.5
   print "#{card_set.first} "
   sleep 1.5
-  print "and #{card_set.last}"
+  print "and #{card_set.last} for a total of #{total(card_set)}"
   puts ""
 end
 
-
-def initial_dealer_display card_set
+def initial_dealer_display(card_set)
   prompt "The Dealers cards are #{card_set.first} and an unknown card"
 end
 
-def display_player_hand card_set
-  prompt "Your cards are #{card_set.join(" and ")}"
+def display_player_hand(card_set)
+  prompt "Your cards are #{card_set.join(' and ')} for a total of #{total(card_set)}"
 end
 
-def display_dealer_hand card_set
-  prompt "The Dealer's cards are #{card_set.join(" and ")}"
+def display_dealer_hand(card_set)
+  prompt "The Dealer's cards are #{card_set.join(' and ')} for a total of #{total(card_set)}"
 end
 
-def card_to_number card
+def card_to_number(card)
   if card.to_i.between?(2, 10)
     card.to_i
   elsif FACE_CARDS.include?(card)
@@ -83,7 +71,7 @@ def card_to_number card
   end
 end
 
-def total card_set
+def total(card_set)
   sum = 0
   card_set.each { |card| sum += card_to_number(card) }
 
@@ -98,36 +86,58 @@ def total card_set
   sum
 end
 
-def busted? card_set
+def busted?(card_set)
   total(card_set) > 21
 end
 
-def blackjack? card_set
+def blackjack?(card_set)
   total(card_set) == 21
 end
 
-def who_won? player_cards, dealer_cards
-  if blackjack?(player_cards) && !blackjack?(dealer_cards)
-    prompt "Congrats, you won!"
-  elsif !busted?(player_cards) && busted?(dealer_cards)
-    prompt "Congrats, you won!"
-  elsif total(player_cards) == total(dealer_cards)
-    prompt "It's a tie!"
-  else
-    prompt "The Dealer won!"
+def dealer_check player_cards, dealer_cards
+  if blackjack?(dealer_cards)
+    prompt "Blackjack!"
+    true
+  elsif busted?(dealer_cards)
+    prompt "The Dealer busted!"
+    true
+  elsif total(dealer_cards) > total(player_cards)
+    true
+  elsif total(dealer_cards) >= 17
+    true
   end
+end
+
+def game_break? card_set
+  if busted?(card_set)
+    prompt "Busted!"
+    true
+  elsif blackjack?(card_set)
+    prompt "Blackjack!"
+    true
+  else
+    false
+  end
+end
+
+def play_again?
+  play_again = true
+  loop do
+    prompt "Do you want to play again? (y/n)"
+    response = gets.chomp.downcase
+    if response.start_with?('y')
+      break
+    elsif response.start_with?('n')
+      play_again = false
+      break
+    end
+  end
+  play_again
 end
 
 # Gameplay
 loop do
-  deck = [['2', 'H'], ['3', 'H'], ['4', 'H'], ['5', 'H'], ['6', 'H'], ['7', 'H'], ['8', 'H'], ['9', 'H'],
-        ['10', 'H'], ['Jack', 'H'], ['Queen', 'H'], ['King', 'H'], ['Ace', 'H'], ['2', 'S'], ['3', 'S'], ['4', 'S'], 
-        ['5', 'S'], ['6', 'S'], ['7', 'S'], ['8', 'S'], ['9', 'S'], ['10', 'S'], ['Jack', 'S'], ['Queen', 'S'], 
-        ['King', 'S'], ['Ace', 'S'], ['2', 'D'], ['3', 'D'], ['4', 'D'], ['5', 'D'], ['6', 'D'], ['7', 'D'], 
-        ['8', 'D'], ['9', 'D'], ['10', 'D'], ['Jack', 'D'], ['Queen', 'D'], ['King', 'D'], ['Ace', 'D'], ['2', 'C'], 
-        ['3', 'C'], ['4', 'C'], ['5', 'C'], ['6', 'C'], ['7', 'C'], ['8', 'C'], ['9', 'C'], ['10', 'C'], 
-        ['Jack', 'C'], ['Queen', 'C'], ['King', 'C'], ['Ace', 'C']]
-
+  deck = initialize_deck
   player_cards = []
   dealer_cards = []
 
@@ -142,67 +152,30 @@ loop do
   initial_player_display(player_cards)
 
   loop do
-    if blackjack?(player_cards)
-      prompt "Blackjack!"
-      break
-    end
-    
-    choice = hit_or_stay
-    break if choice == "stay"
-    
+    break if game_break?(player_cards)
+    break if hit_or_stay == 'stay'
+
     deal(player_cards, deck)
-    
-    if busted?(player_cards)
-      prompt "You busted!"
-      break
-    elsif blackjack?(player_cards)
-      prompt "Blackjack!"
-      break
-    end
+
+    break if game_break?(player_cards)
 
     display_player_hand(player_cards)
     sleep 2
-    prompt "Your total is #{total(player_cards)}"
   end
 
   display_dealer_hand(dealer_cards)
   sleep 2
   loop do
-
-    if blackjack?(dealer_cards)
-      prompt "Blackjack!"
-      break
-    elsif busted?(dealer_cards)
-      prompt "The Dealer busted!"
-      break
-    elsif total(dealer_cards) > total(player_cards)
-      break
-    elsif total(dealer_cards) >= 17
-      break
-    end
+    break if game_break?(dealer_cards) || total(dealer_cards) >= 17
 
     deal(dealer_cards, deck)
     display_dealer_hand(dealer_cards)
     sleep 2
-    prompt "The Dealer's total is #{total(dealer_cards)}"
   end
 
   sleep 2
-  who_won?(player_cards, dealer_cards)
 
-  play_again = true
-  loop do
-    prompt "Do you want to play again? (y/n)"
-    response = gets.chomp.downcase
-    if response.start_with?('y')
-      break
-    elsif response.start_with?('n')
-      play_again = false
-      break
-    end
-  end
-
-  break if play_again == false
+  break if !play_again?
 end
 
 prompt "Thanks for playing!"
